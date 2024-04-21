6a9494c2d1d388c3b2b5d2865590c712dc1c24e5
MateusN17 <mateusnascioliveira@gmail.com>
2024-04-21 13:24:15 -0300

    kkk

--- carro.py
+++
@@ -1,8 +0,0 @@
-from classes import Carro
-
-if __name__=="__main__":
-    palio=Carro(placa=input("Insira a placa: "), modelo=input("Insira o modelo: "), ano=input("Insira o ano: "))
-    vectra=Carro(placa=input("Insira a placa: "), modelo=input("Insira o modelo: "), ano=input("Insira o ano: "))
-    duster=Carro(placa=input("Insira a placa: "), modelo=input("Insira o modelo: "), ano=input("Insira o ano: "))
-    
-print(palio)
