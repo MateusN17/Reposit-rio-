f786ce7005f3eb03e44d049542143f154e913fcb
MateusN17 <mateusnascioliveira@gmail.com>
2024-05-03 11:21:44 -0300

    kskask

--- Kivy/video.py
+++ Kivy/video.py
@@ -1,8 +1,9 @@
 from kivy.app import App
 from kivy.uix.video import Video
-class MinhaApp(App):
-    def build(self):
-        return Video(source="\Users\mateu\Downloads\PXL_20240417_160011210~2.mp4")
 
-if __name__=="__main__":
-    MinhaApp().run()
+class VideoPlayer(App):
+    def build(self):
+        return Video(source='', state='play')
+
+if __name__ == '__main__':
+    VideoPlayer().run()
